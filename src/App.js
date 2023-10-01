4ae56759b64e59804e7bdac60df67d9312708e61
RakeshKunapuli <143953416+RakeshKunapuli@users.noreply.github.com>
2023-10-01 21:00:55 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+// import Header from "./Header"
+import Videos from "./VideoData"
+import NavBar from './Navbar';
+
+function App() {
+  return (   
+    <div>
+       <NavBar/>
+       <Videos />
+
+      </div>
+    )       
+}
+
+export default App;
+
+
---
+++ src/VideoData.js
@@ -0,0 +1,138 @@
+function Videos(){
+    let video = [
+        { id: 1,
+                 title: "What if everything we knew about black hole was wrong?",
+                 thumbnailUrl: "https://i.ytimg.com/vi/1M3q1y5rg0w/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLAUXFKeR4J1u2hoP5sXk2imZYDurQ",
+                 likes: 118,
+                 views:125,
+                 year:"2 years ago",
+                 channelName: "What If",
+                 channelLogoUrl: "https://yt3.googleusercontent.com/7kxh8K24lAjoP_1ZWKSi3YdpURnqEjZ4pn-2A7ZuEqnvmrflghCCSkSZArhApF7Kfxz0NcuPEQ=s900-c-k-c0x00ffffff-no-rj",
+                },
+                {id: 2,
+                    title: "iPhone 15 Pro Review: The Good, The Bad, & The Ugly!",
+                    thumbnailUrl: "https://cdn2.myvidster.com/user/thumbs/4b5b6034aba014686e9244a8fbbfcd6e_1.jpg",
+                    likes: 40,
+                 views:5.2,
+                 year:"2 months ago",
+                    channelName: "Marques Brownlee",
+                    channelLogoUrl: "https://yt3.googleusercontent.com/lkH37D712tiyphnu0Id0D5MwwQ7IRuwgQLVD05iMXlDWO-kDHut3uI4MgIEAQ9StK0qOST7fiA=s176-c-k-c0x00ffffff-no-rj"
+                } ,
+                { id: 3,
+                    title: "$1 Vs $100,000,000 Car!",
+                    thumbnailUrl: "https://i.ytimg.com/vi/KrLj6nc516A/maxresdefault.jpg",
+                    likes: 541,
+                 views:12,
+                 year:"1 years ago",
+                    channelName: "MrBeast",
+                    channelLogoUrl: "https://yt3.ggpht.com/ytc/APkrFKY455xp16s2AIHalRjK60zas-DitxAHmRjQsXPE2A=s48-c-k-c0x00ffffff-no-rj"
+                },
+    
+                { id: 4,
+                    title: "HYDRAULIC PRESS VS TITANIUM AND CARBON FIBER PIPE",
+                    thumbnailUrl: "https://socialcounts.org/_next/image?url=https%3A%2F%2Fi.ytimg.com%2Fvi%2FPlvyZ1r1DCM%2Fhqdefault.jpg&w=828&q=75",
+                    likes: 125,
+                    views:6.4,
+                    year:"9 months ago",
+                    channelName: "Crazy Hydraulic Press",
+                    channelLogoUrl: "https://yt3.ggpht.com/fBFCjEEP6e96EWCKY7sZ813RX2RwYDwsLVO875PTB6Jy6LgDYw1NqPeQW5IYwRyjyJ-zfs8OxGI=s48-c-k-c0x00ffffff-no-rj"
+                }
+        
+                ,{ id: 5,
+                    title: "ANIMAL Teaser (Telugu): Ranbir Kapoor| Sandeep Reddy Vanga| Bhushan Kumar",
+                    thumbnailUrl: "https://i.ytimg.com/vi/o-_nvFRIF34/maxresdefault.jpg",
+                    likes: 119,
+                 views:1.9,
+                 year:"5 months ago",
+                    channelName: "T-Series Telugu",
+                    channelLogoUrl: "https://yt3.ggpht.com/ytc/APkrFKYkn9Q9VM_xwhxA6ik3lGkWyRZ5N62fLHtuuUuy2w=s48-c-k-c0x00ffffff-no-rj"
+                }
+        
+                ,{ id: 6,
+                    title: "Jawan | Official Telugu Trailer | Shah Rukh Khan | Atlee | Nayanthara | Vijay S | Deepika P |Anirudh",
+                    thumbnailUrl: "https://thetelugufilmnagar.com/wp-content/uploads/2023/08/Jawan-Trailer-Out-Now.webp",
+                    likes: 52,
+                 views:2.6,
+                 year:"3 months ago",
+                    channelName: "Red Chillies Entertainment",
+                    channelLogoUrl: "https://yt3.ggpht.com/FU332R7JMjZaRm1YGvk8NnHJ1C9zW_yLpXz0GkHqGXV0bDRaAuulxt7s2TEJkN05FojDn2I5JuU=s48-c-k-c0x00ffffff-no-rj"
+                }
+        
+                ,{  id: 7,
+                    title: "i am now supreme leader",
+                    thumbnailUrl: "https://i.ytimg.com/vi/sK37SolFV8I/maxresdefault.jpg",
+                    likes: 18,
+                 views:6.9,
+                 year:"2 years ago",
+                    channelName: "PewDiePie",
+                    channelLogoUrl: "https://yt3.ggpht.com/5oUY3tashyxfqsjO5SGhjT4dus8FkN9CsAHwXWISFrdPYii1FudD4ICtLfuCw6-THJsJbgoY=s176-c-k-c0x00ffffff-no-rj-mo"
+                }
+        
+                ,{ id: 8,
+                    title: "SPY x FAMILY Season 2 | OFFICIAL TRAILER",
+                    thumbnailUrl: "https://i.ytimg.com/vi/W2tMsaAVjD0/maxresdefault.jpg",
+                    likes: 8,
+                 views:1.5,
+                 year:"1 year ago",
+                    channelName: "Crunchyroll Collection",
+                    channelLogoUrl: "https://yt3.ggpht.com/ytc/APkrFKaVnvc8KleIdBDnGmKV6ARBeuArPZaHuE2wyYWlRw=s48-c-k-c0x00ffffff-no-rj"
+                }
+        
+                ,{  id: 9,
+                    title: "New Cupra Dark Rebel is a Glowing Supercar",
+                    thumbnailUrl: "https://i.ytimg.com/vi/g3ycZvhK9Ro/maxresdefault.jpg",
+                    likes: 618,
+                    views:12,
+                    year:"1 year ago",
+                    channelName: "Supercar Blondie",
+                    channelLogoUrl: "https://yt3.ggpht.com/Tu5e4QZQM1SrWBqyDrx7m9duC3H_htsKsiaH-xMBx9qfLdVwIh0ZcZf3xgZUhBbrotCFvGoXdMI=s176-c-k-c0x00ffffff-no-rj-mo"
+                }
+        
+                ,{id: 10,
+                    title: "One Piece Episode 1078 English Subbed HD1080 ( FIXSUB",
+                    thumbnailUrl: "https://www.viv.co.id/uploads/large/12a8763203a46890b86337f69fccf9d5.PNG",
+                    likes: 18,
+                 views:1.9,
+                 year:"1 years ago",
+                    channelName: "unique update polapain",
+                    channelLogoUrl: "https://yt3.ggpht.com/AdiYoxsTOXsDTzWIWK9K86GAzgYQKc8fo7ZibEUmLjJEyfyheEZtmta1iXPmbxv_1mFEnv6Pcw=s48-c-k-c0x00ffffff-no-rj"
+                }
+        
+                ,{ id: 11,
+                    title: "Charlie Day on Rob McElhenney & Ryan Reynolds’ Friendship, Going to a Wrexham Game & Fool's Paradise",
+                    thumbnailUrl: "https://i.ytimg.com/vi/lJxdlhY-1n8/maxresdefault.jpg",
+                    likes: 118,
+                 views:125,
+                 year:"2 years ago",
+                    channelName: "Jimmy Kimmel Live",
+                    channelLogoUrl: "https://yt3.googleusercontent.com/ytc/APkrFKbTYm-RUBcq7UiswcuBilGCIJJ2js6lkzy8xK8ajdw=s176-c-k-c0x00ffffff-no-rj-mo"
+                } ,
+                    
+                {   id: 12,
+                    title: "How a Traded-In Apple iPhone Gets Refurbished | WSJ",
+                    thumbnailUrl: "https://i.ytimg.com/vi/WHqxBlFuIXA/maxresdefault.jpg",
+                    likes: 15,
+                 views:1.9,
+                 year:"4 months ago",
+                    channelName: "Wall Street Journal",
+                    channelLogoUrl: "https://yt3.ggpht.com/ytc/APkrFKZeja1fq6FdUP3MSV79zNZcBevYxdKtDxP5Ydn5gw0=s48-c-k-c0x00ffffff-no-rj"
+                },
+        
+    ]
+    return (
+             video.map((video,i) =>(
+     <div className="video-card">
+         <div className = "namelogo">
+ <img className="Logo" src={video.channelLogoUrl} alt={video.title}/> <span><b>{video.channelName}</b></span>
+ <img className="verified" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/YT_Official_Verified_Checkmark_Circle.svg/2048px-YT_Official_Verified_Checkmark_Circle.svg.png" alt={video.title}/>
+ </div>
+ <img className="thumbnail" src={video.thumbnailUrl} alt={video.title}/>
+ <div className="matter">
+ <h4>{video.title}</h4>
+ <p>{video.views}M views<span></span>{video.likes}K likes<span>{video.year}</span></p>
+ </div>
+</div>
+ )))}
+
+        export default Videos;
+    
